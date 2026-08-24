8b74e801fc1d7ac66f90705ac022feef39736687
akku1139 <akku1139@users.noreply.github.com>
2026-08-25 04:17:27 +0900

    debug: track fork develop branch for upstream fix test

--- hazkey-server/Package.swift
+++ hazkey-server/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/7ka-hiira/AzooKeyKanaKanjiConverter",
-            branch: "8b4befc",
+            branch: "develop",
             traits: [.trait(name: "Zenzai", condition: .when(traits: ["ZenzaiSupport"]))]),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.27.0"),
     ],
